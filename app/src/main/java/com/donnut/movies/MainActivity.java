5ec63af1a1c053f607144d6a4932bc4b8d272fb8
fdonnut <36168787+fdonnut@users.noreply.github.com>
2022-12-08 15:15:04 +0300

    dependency and POJO

--- app/src/main/java/com/donnut/movies/MainActivity.java
+++ app/src/main/java/com/donnut/movies/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TOKEN = "09YME5T-R4EM7VZ-QCXKR7Y-RQ7Y26Y";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
