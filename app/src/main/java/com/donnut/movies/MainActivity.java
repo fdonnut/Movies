859e0f9afe32e49bb6a0a7de83f503e146ad8bbf
fdonnut <36168787+fdonnut@users.noreply.github.com>
2022-12-08 22:13:00 +0300

    adapter

--- app/src/main/java/com/donnut/movies/MainActivity.java
+++ app/src/main/java/com/donnut/movies/MainActivity.java
@@ -3,6 +3,8 @@ package com.donnut.movies;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -11,19 +13,25 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
     private MainViewModel viewModel;
     private static final String TOKEN = "09YME5T-R4EM7VZ-QCXKR7Y-RQ7Y26Y";
+
+    private RecyclerView recyclerViewMovies;
+    private MoviesAdapter moviesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerViewMovies = findViewById(R.id.recyclerViewMovies);
+        moviesAdapter = new MoviesAdapter();
+        recyclerViewMovies.setAdapter(moviesAdapter);
+        recyclerViewMovies.setLayoutManager(new GridLayoutManager(this, 2));
         viewModel = new ViewModelProvider(this).get(MainViewModel.class);
         viewModel.getMovies().observe(this, new Observer<List<Movie>>() {
             @Override
             public void onChanged(List<Movie> movies) {
-                Log.d(TAG, movies.toString());
+                moviesAdapter.setMovies(movies);
             }
         });
         viewModel.loadMovies();
