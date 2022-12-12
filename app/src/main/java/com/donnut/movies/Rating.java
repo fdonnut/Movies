f20501d4dace720eb1959a80beea9fa6cbe21f85
fdonnut <36168787+fdonnut@users.noreply.github.com>
2022-12-12 14:03:35 +0300

    movie detail

--- app/src/main/java/com/donnut/movies/Rating.java
+++ app/src/main/java/com/donnut/movies/Rating.java
@@ -2,7 +2,9 @@ package com.donnut.movies;
 
 import com.google.gson.annotations.SerializedName;
 
-public class Rating {
+import java.io.Serializable;
+
+public class Rating implements Serializable {
 
     @SerializedName("imdb")
     private double imdb;
